42a61ed17fbd92d16369c83b2128207d4a828857
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-03-13 21:32:34 +0100

    Don't indicate error if signed-out user signs out

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -37,3 +37,36 @@ export function authMiddleware(req: ProtectedRequest, res: Response, next: NextF
         res.status(401).json({ message: "Un-authorized, invalid token"})
     }
 }
+
+export function authSignOutMiddleware(req: ProtectedRequest, res: Response, next: NextFunction) {
+    // Check that req.cookies is defined
+    if (!req.cookies) {
+        res.status(401).json({message: "Un-authorized, missing Cookies header"});
+        return;
+    }
+
+    const token = req.cookies.token;
+
+    // Check if user is already signed out
+    if(!token) {
+        res.status(200).json({message: "User signed-out successfully"});
+        return;
+    }
+
+    // Check JWT SECRET is defined
+    if(!process.env.JWT_SECRET) {
+        res.status(500).json({ message: "JWT SECRET is not defined"})
+        return;
+    }
+
+    // Check that token is valid
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload
+        req.user = decoded;
+        console.log("req.user", req.user);
+        next();
+    } catch(error) {
+        console.log(error);
+        res.status(401).json({ message: "Un-authorized, invalid token"})
+    }
+}
--- src/routes/signOutRoutes.ts
+++ src/routes/signOutRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express"
 import { signOut } from "../controllers/signOutController";
-import { authMiddleware } from "../middleware/authorize";
+import { authSignOutMiddleware } from "../middleware/authorize";
 
 const router = express.Router()
 
 // CRUD for users
-router.post("/", authMiddleware, signOut);
+router.post("/", authSignOutMiddleware, signOut);
 
 export default router;
